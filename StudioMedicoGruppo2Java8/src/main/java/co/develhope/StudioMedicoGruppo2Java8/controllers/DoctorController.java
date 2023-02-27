a515866692ec9bf5c1bd83d1c2b17c444cccef71
slendy98DX <marco.setaro93@gmail.com>
2023-02-27 16:52:09 +0100

    creation of sql file

--- StudioMedicoGruppo2Java8/src/main/java/co/develhope/StudioMedicoGruppo2Java8/controllers/DoctorController.java
+++ StudioMedicoGruppo2Java8/src/main/java/co/develhope/StudioMedicoGruppo2Java8/controllers/DoctorController.java
@@ -26,5 +26,4 @@ public class DoctorController {
         return doctorRepository.findAll();
     }
 
-
 }
--- StudioMedicoGruppo2Java8/src/main/java/co/develhope/StudioMedicoGruppo2Java8/entities/Doctor.java
+++ StudioMedicoGruppo2Java8/src/main/java/co/develhope/StudioMedicoGruppo2Java8/entities/Doctor.java
@@ -1,7 +1,6 @@
 package co.develhope.StudioMedicoGruppo2Java8.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
